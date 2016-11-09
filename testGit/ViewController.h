5400528b1408c68c24e7722791d854e243598c39
liujia <liu@test.local>
2016-11-09 15:59:37 +0800

    Initial Commit

---
+++ testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by liujia on 16/11/9.
+//  Copyright © 2016年 LC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testGit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  testGit
+//
+//  Created by liujia on 16/11/9.
+//  Copyright © 2016年 LC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
